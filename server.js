f12f24230327f201860d50df102a1d075dce78c8
Ojiromayo <labeodanojiromayo@gmail.com>
2024-09-24 18:46:57 +0100

    Assignment completed

---
+++ server.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const morgan = require("morgan")
+const app = express();
+const port = 3000;
+
+const RESTAURANT = {
+    name: 'The Green Byte Bistro',
+    isOpen: true,
+    address: '742 Evergreen Rd, Mapleview, OS 45502',
+    phone: '555-321-9876',
+    menu: [
+      { 
+        id: 1,
+        name: 'Quantum Quinoa Mushroom Burger',
+        price: 13.00,
+        rating: 4,
+        category: 'mains',
+        details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+      },
+      { 
+        id: 2,
+        name: 'Binary Berry Cheesecake',
+        price: 10.11,
+        rating: 3,
+        category: 'desserts',
+        details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+      },
+      { 
+        id: 3,
+        name: 'Recursive Rigatoni',
+        price: 17.00,
+        rating: 5,
+        category: 'mains',
+        details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+      },
+      { 
+        id: 4,
+        name: 'Pumpkin Pi Squared',
+        price: 3.14,
+        rating: 5,
+        category: 'desserts',
+        details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+      },
+      { 
+        id: 5,
+        name: 'Fibonacci String Bean Fries',
+        price: 11.23,
+        rating: 5,
+        category: 'sides',
+        details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+      }
+    ]
+  }
+  
+
+
+
+//.................... Middlewear
+app.use(morgan("dev"));
+
+
+//..................... Routes
+// Home
+app.get('/', (req, res) => {
+  res.render("home.ejs", {
+    resturant:RESTAURANT
+  });
+});
+
+// Menu
+app.get('/menu', (req, res) => {
+    res.render("menu.ejs", {
+        menu: RESTAURANT.menu
+    });
+});
+
+
+// Category
+app.get('/menu/:category', (req, res) => {
+    const {category} = req.params
+    const {menu} = RESTAURANT
+    const mains = menu.filter((items) => items.category === "mains")
+    const sides = menu.filter((items) => items.category === "sides")
+    const desserts = menu.filter((items) => items.category === "desserts")
+    
+    let items = []
+
+    if(category === "mains") {
+        items = [...mains]
+    } else if (category === "sides") {
+        items = [...sides]
+    } else if (category === "desserts") {
+        items = [...desserts]
+    }
+    
+    res.render("category.ejs", {
+        category: category,
+        items: items,
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`App listning on port ${port}`)
+});
